312450a49b6bbfc76e2a37e18ef12eccb6a5ba30
joaothallys <jlemes796@gmail.com>
2025-03-08 14:20:20 -0300

    removendo logica Leaderboard

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -4,13 +4,9 @@ import React from "react";
 const Leaderboard: NextPage = () => {
   return (
     <div>
-      <div className="flex justify-center gap-3 pt-14 md:ml-24 md:p-6 md:pt-10 lg:ml-64 lg:gap-12">
-        <div className="flex w-full max-w-xl flex-col items-center gap-5 pb-28 md:px-5">
-          <h1 className="text-center text-2xl font-bold text-gray-700">
-            Leaderboard Page
-          </h1>
-        </div>
-      </div>
+      <h1 className="text-center text-2xl font-bold text-gray-700">
+        Leaderboard Page
+      </h1>
     </div>
   );
 };
